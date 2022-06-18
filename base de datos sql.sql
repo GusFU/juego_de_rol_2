320bd17f9cce3abe305b2f08535c9a87933fac78
GusFU <guscp84@gmail.com>
2022-06-18 23:46:41 +0200

    insertar registro en la base de datos

--- base de datos sql.sql
+++ base de datos sql.sql
@@ -13,12 +13,12 @@ CREATE TABLE Usuarios (
     telefono VARCHAR(15) NOT NULL,
     email VARCHAR(100) NOT NULL,
     direccion1 VARCHAR(500) NOT NULL,
-    direccion2 VARCHAR(500),
-    direccion3 VARCHAR(500),
+    direccion2 VARCHAR(500) default null,
+    direccion3 VARCHAR(500)default null,
     contrasena VARCHAR(100) NOT NULL,
     PRIMARY KEY(id)
 );
-INSERT INTO Usuarios VALUES(NULL,"sergio aaa ddd","12345678a",false,123456789,"kkkkkk@kkkk.com","recoletos 15",null,null,"1234");
+INSERT INTO Usuarios VALUES(NULL,"sergio aaa ddd","12345678a",true,123456789,"kkkkkk@kkkk.com","recoletos 15",null,null,"1234");
 INSERT INTO Usuarios VALUES(NULL,"gustavo aaa ddd","87654321a",false,123456789,"gggggggg@kkkk.com","recoletos 112",null,null,"1234");
 select * from Usuarios;
 
@@ -32,7 +32,7 @@ CREATE TABLE Login (
     FOREIGN KEY(fk_id_usuario) REFERENCES Usuarios(id)
 );
 insert into Login values(null,"kkkkkk@kkkk.com","1234",1);
-insert into Login values(null,"gggggggg@kkkk.com","1234",3);
+insert into Login values(null,"gggggggg@kkkk.com","1234",2);
 select * from Login;
 #DROP TABLE Tarjetas
 CREATE TABLE Tarjetas (
